4e2dd1de118431b09ac9bd978a291c4333b9b4cd
chrenx <chrenx@umich.edu>
2021-10-08 16:57:19 -0400

    update

--- insta485/js/post.jsx
+++ insta485/js/post.jsx
@@ -1,54 +1,114 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import moment from 'moment';
 
-class Post extends React.Component {
-  /* Display number of image and post owner of a single post
-   */
 
-  constructor(props) {
-    // Initialize mutable state
-    super(props);
-    this.state = { imgUrl: '', owner: '' };
-  }
+class IndividualPost extends React.Component {
+    constructor(props) {
+        super(props);
+        // this.state = { comments: props.comments, created: props.created,
+        //                imgUrl: props.imgUrl, likes: props.likes,
+        //                owner: props.owner, ownerImgUrl: , ownerShowUrl: '',
+        //                postShowUrl: '', postid: null, url: '' };
+    }
 
-  componentDidMount() {
-    // This line automatically assigns this.props.url to the const variable url
-    const { url } = this.props;
+    componentDidMount() {
+        // this.setState({
+        //     comments: this.props.comments,
+        //     created: this.props.created,
+        //     imgUrl: this.props.imgUrl,
+        //     likes: this.props.likes,
+        //     owner: this.props.owner,
+        //     ownerImgUrl: this.props.ownerImgUrl,
+        //     ownerShowUrl: this.props.ownerShowUrl,
+        //     postShowUrl: this.props.postShowUrl,
+        //     postid: this.props.postid,
+        //     url: this.props.url,
+        // });
+    }
 
-    // Call REST API to get the post's information
-    fetch(url, { credentials: 'same-origin' })
-      .then((response) => {
-        if (!response.ok) throw Error(response.statusText);
-        return response.json();
-      })
-      .then((data) => {
-        this.setState({
-          imgUrl: data.img_url,
-          owner: data.owner,
-        });
-      })
-      .catch((error) => console.log(error));
-  }
+    render() {
+        
+        return (
+            // 放头像
+            // 放用户名
+            <div className='middlePart'>
+                <a className='hyperlinkstyle' href={this.props.ownerShowUrl} >
+                    <img src={this.props.ownerImgUrl} alt='image for user' width='40' style={{position:'absolute'}} />
+                    <div style={{position:'absolute', top:'15px', left:'50px'}}>
+                        <b>{this.props.owner}</b>
+                    </div>
+                </a>
+                <a className='hyperlinkstyle' href={this.props.postShowUrl} >
+                    <div style={{position:'absolute', top:'15px', right:'0px', color:'gray'}}>
+                        {moment(this.props.created).fromNow()}
+                    </div>
+                </a>
+                <br /><br /><br />
+                <img className="post1" src={this.props.imgUrl} alt='post image' />
+                <br /><br /><br />
+            </div>
 
-  render() {
-    // This line automatically assigns this.state.imgUrl to the const variable imgUrl
-    // and this.state.owner to the const variable owner
-    const { imgUrl, owner } = this.state;
-
-    // Render number of post image and post owner
-    return (
-      <div className="post">
-        <img src={imgUrl} alt="descripition of url" />
-        <p>
-          {owner}
-        </p>
-      </div>
-    );
-  }
+            // 放照片
+            // 放like button
+            // 放几个like
+            // 放所有comments
+            // 放commets submit
+        );
+    }
 }
 
-Post.propTypes = {
-  url: PropTypes.string.isRequired,
+
+class Posts extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { results: [] };
+    }
+
+    componentDidMount() {
+        const { url } = this.props;
+
+        fetch(url, { credentials: 'same-origin' })
+        .then((response) => {
+            if (!response.ok) throw Error(response.statusText);
+            return response.json();
+        })
+        .then((data) => {
+            this.setState({
+                results: data.results,
+            });
+        })
+        .catch((error) => console.log(error));
+    }
+
+    render() {
+        const sub_result = [];
+        this.state.results.forEach((sub) => {
+            sub_result.push(
+                <IndividualPost
+                    comments={sub.comments}
+                    created={sub.created}
+                    imgUrl={sub.imgUrl}
+                    likes={sub.likes}
+                    owner={sub.owner}
+                    ownerImgUrl={sub.ownerImgUrl}
+                    ownerShowUrl={sub.ownerShowUrl}
+                    postShowUrl={sub.postShowUrl}
+                    postid={sub.postid}
+                    url={sub.url}
+                    key={sub.postid} />
+            );
+        });
+        return (
+            <ul>{sub_result}</ul>
+        );
+    }
+}
+
+
+Posts.propTypes = {
+    url: PropTypes.string.isRequired,
 };
 
-export default Post;
+
+export default Posts;
